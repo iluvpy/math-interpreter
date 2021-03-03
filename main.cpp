8e7ee651feb0d8f7fa24a4690274c6e689280e7d
iluvpy <miomanns265@gmail.com>
2021-03-03 21:32:04 +0100

    added first bit of code

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+
+#include <unistd.h>
+
+
+class interpreter {
+
+public:
+    
+    void run() {
+
+    }
+
+    void loop() {
+
+    }
+
+    void calculate() {
+        
+    }
+
+
+private:
+    std::string expression;
+
+};
+
+int main() {
+
+
+    return 0;
+}
