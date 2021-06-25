2bde09fbb255dd02d2196924bc39b37f719108ac
iluvpy <miomanns265@gmail.com>
2021-06-25 23:58:52 +0200

    added string digit

--- src/include/utils.c
+++ src/include/utils.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "utils.h"
 
+// returns true if string is a digit 
+// supports floating point numbers
+bool string_digit(char *s) {
+    int digits = -strlen(s);
+    for (int i = 0; i < strlen(s); i++) {
+        digits += (isdigit(s[i]) || s[i] == '.' || s[i] == ',') ? 1 : 0;
+    }
+    return digits == 0;
+}
--- src/include/utils.h
+++ src/include/utils.h
@@ -2,16 +2,11 @@
 #define UTILS_H
 
 #include <string.h>
+#include <stdbool.h>
 #include "defines.h"
 
-
-#define IF_QUIT(t) t == "q" || t == "quit"
-
-// returns the percentage of similarity (1 is 100% and 0 would be 0%)
-f32 is_similar(char *s1, char *s2) {
-    int l1 = strlen(s1);
-    int l2 = strlen(s2); 
-    int sims = 0;
-}
+// returns true if string is a digit 
+// supports floating point numbers
+bool string_digit(char *s);
 
 #endif // UTILS_H
