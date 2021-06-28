ae2e71e8b5e7dd89139af8c7abf314b30af24199
iluvpy <miomanns265@gmail.com>
2021-06-28 02:07:19 +0200

    BUUUGS

--- src/test.c
+++ src/test.c
@@ -16,7 +16,11 @@ void print_res(char *t, bool res) {
     }
 }
 
-
+void printTest(char *name) {
+    printGreen("------ ");
+    printGreen(name);
+    printlnGreen(" ------");
+}
 int main(int argc, char const *argv[])
 {
     printf("starting test...\n");
@@ -24,20 +28,23 @@ int main(int argc, char const *argv[])
     char d2[] = "123.65432"; // digit
     char d3[] = "234d2rfgs"; // not a digit 
     char *result_text = NULL;
-    char *red = "red";
+    char *color_text;
     char *rev_test = "hello";
+
+    printTest(" text test ");
     // testing colors
     printlnGreen("green text");
     printlnRed("red text");
 
-    getColorText(red, KRED);
-    printf("testing get color text; %s <- red?\n", red);
+    getColorText(color_text, "Red", KRED);
+    printf("testing get color text; %s <- red?\n", color_text);
+    free(color_text);
     char rev_cpy[strlen(rev_test)];
     strcpy(rev_cpy, rev_test);
     rev_str(rev_test);
     printf("testing rev str; '%s' becomes: %s\n", rev_cpy, rev_test);
     
-    printlnGreen("------ Utils ------\n");
+    printTest("Util test");
     bool res1 = string_digit(d1);
     bool res2 = string_digit(d2);
     bool res3 = string_digit(d3);
@@ -46,14 +53,14 @@ int main(int argc, char const *argv[])
     print_res("string digit d2 T: ", res2);
     print_res("string digit d3 F: ", res3);
 
-    printlnGreen("------ Vector ------\n");
+    printTest("Vector test");
     Vector v;
     initV(&v);
     printf("initalized vector, length: %d\n", v.length);
     appendV(&v, 100);
     print_res("appended 100 to vector successfully: ", (Vget(&v, 0) == 100));
     deleteVec(&v);
-    print_res("deleting vector successfully: ", (v.ptr == NULL));
+    print_res("deleted vector successfully: ", (v.ptr == NULL));
     free(result_text);
     return 0;
 }
