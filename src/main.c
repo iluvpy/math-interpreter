7bf7e836a5940353ddfa967569ab24f1341bf061
iluvpy <miomanns265@gmail.com>
2021-07-03 19:44:15 +0200

    testing cstring library

--- src/main.c
+++ src/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
 #include "cstr.h"
+#include "console.h"
 
 int main(int argc, char **argv) {
+    size_t max_chars = 100;
+    printf("text 1 (max characters %ld): ", max_chars);
+    cstr *text1 = minput(100);
+    printf("text 2 (max characters %ld): ", max_chars);
+    cstr *text2 = minput(100);
 
-    cstr *str = get_cstr("hello ");
-    append_cstr(str, "world");
-    printf("%s\n", getcstr(str));
 
-    del_cstr(str);
+    cstr *result = add_cstr(text1, text2);
+
+    printf("result: \n%s", getcstr(result));
+
+    del_cstr(text1);
+    del_cstr(text2);
+    del_cstr(result);
     printf("exiting\n");
     return 0;
 }
