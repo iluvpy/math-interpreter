e9845052d823762e14eae39e222f9f352e3bf374
iluvpy <miomanns265@gmail.com>
2021-07-04 20:28:57 +0200

    added dinput, cstrdinput and clear console functions

--- src/include/console.c
+++ src/include/console.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "cstr.h"
 #include "console.h"
+
+
+// input 
 
 // max input
 // set max bytes
@@ -10,11 +13,46 @@ cstr *minput(size_t max) {
     char *str = malloc(max);
     str = fgets(str, max, stdin);
     if (str == NULL) {return NULL;}
-    return cstr_fallocstr(str);
+    return cstr_from_allocstr(str);
 }
 
  // gets input dynamically
 char *dinput() {
+    size_t len = DINP_LEN;
+    char *inp = malloc(len);
+    int i = 0;
+    char c;
+    while ((c = getchar()) && c != '\n') {
+        inp[i] = c;
+        i++;
+        if (i >= len) {
+            len += DINP_LEN;
+            inp = realloc(inp, len);
+        }
+    }
+    // resize to correct size
+    size_t size = strlen(inp);
+    inp = realloc(inp, size);
+    return inp;
+}
 
+// works like dinput but returns cstr instead
+cstr *cstrdinput() {
+    cstr *inp = cstr_from_allocstr(dinput());
+}
+
+
+// output 
+
+
+
+// other
+
+void clear_console() {
+#ifdef WIN32_
+    system("cls");
+#else
+    system("clear");
+#endif
 }
 
--- src/include/console.h
+++ src/include/console.h
@@ -2,21 +2,35 @@
 #define CONSOLE_C
 
 #include "cstr.h"
+
+#define DINP_LEN 512
+
 // input
 
 // max input
 // set max bytes
 cstr *minput(size_t max);
 
-char *dinput(); // gets input dynamically
+
+// gets input dynamically
+char *dinput();
+// works like dinput but returns cstr instead
+
+
+cstr *cstrdinput(); 
 
 
 // output
 
-
+void print_color() {
+    
+}
 
 
 // other
+
+
+void clear_console();
 
 #endif // CONSOEL_H
 
