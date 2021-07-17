6e00cf56ae8839ad28d108860137b5cd9fbdeced
iluvpy <miomanns265@gmail.com>
2021-07-17 18:18:13 +0200

    simpler test

--- src/test.c
+++ src/test.c
@@ -18,26 +18,16 @@ void print_section(char *name) {
 }
 
 int main(void) {
-	srand(time(NULL));
-	char chars[10][2] = {"a.", "b9", "c1", "d0", "00", "5j", "0?", "i#", "6x", "uh"};
+
 	print_section("string vector test");
 
 	svector *vec = generate_svec();
-	size_t vector_size = 100;
-	for (int i = 0; i < vector_size; i++) {
-		cstr *temp_str = get_cstr(chars[ rand() % 10 ]);
-		for (int j = 0; j < 10; j++) {
-			append_str(temp_str, chars[ rand() % 10 ]);
-		}
-		printf("added %s to vector index %d", getstr(temp_str), i);
-		svec_append(vec, temp_str);
-		del_cstr(temp_str);
-	}
+	svec_appendc_str(vec, "hello");
+	svec_appendc_str(vec, "world");
+	svec_appendc_str(vec, "i like cheese");
+	svec_appendc_str(vec, "h4");
+	svec_appendc_str(vec, "hi");
 
-	for (int i = 0; i < vector_size; i++) {
-		printf("index %d is %s", i, getstr(svec_get(vec, i)));
-	}
-
-	del_svec(vec);
+	printf("last %s\n", getstr(svec_get(vec, vec->length-1)));
 	return 0;
 }
