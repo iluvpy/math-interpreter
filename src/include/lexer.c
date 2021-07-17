b2d666f25eeffd55afc8e5f6fc92ba3551a02a49
iluvpy <miomanns265@gmail.com>
2021-07-17 18:18:05 +0200

    added lexer files

--- src/include/lexer.c
+++ src/include/lexer.c
@@ -1,2 +1,13 @@
 
 #include "lexer.h"
+
+
+// transforms a mathematical expression into a simple start - to finish string of 
+// tokens seperated by spaces
+cstr *get_tokens(cstr *m_expression) {
+
+	// find first to execute like parentethis (1+2)*3 1+2 needs to be executed before *3
+
+	// transform to tokens
+	return ((cstr*)0);
+}
--- src/include/lexer.h
+++ src/include/lexer.h
@@ -2,4 +2,7 @@
 
 #include "cstr.h"
 
+
+// transforms a mathematical expression into a simple start - to finish string of 
+// tokens seperated by spaces
 cstr *get_tokens(cstr *m_expression);
