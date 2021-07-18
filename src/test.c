d11d055052244a4c7f2f64db875ab22b47ef6c07
iluvpy <miomanns265@gmail.com>
2021-07-18 23:46:03 +0200

    testing new code

--- src/test.c
+++ src/test.c
@@ -22,12 +22,24 @@ int main(void) {
 	print_section("string vector test");
 
 	svector *vec = generate_svec();
+	cstr *str = get_cstr("hello");
 	svec_appendc_str(vec, "hello");
 	svec_appendc_str(vec, "world");
 	svec_appendc_str(vec, "i like cheese");
 	svec_appendc_str(vec, "h4");
 	svec_appendc_str(vec, "hi");
+	svec_append(vec, str);
 
 	printf("last %s\n", getstr(svec_get(vec, vec->length-1)));
+
+	int n = 152566;
+	printf("there are %zu digits in %d\n", get_intlen(n), n);
+
+	printf("input: ");
+	cstr *input = minput(6);
+	printf("input was '%s'\n", getstr(input));
+	del_cstr(input);
+	del_svec(vec);
+	del_cstr(str);
 	return 0;
 }
