3cc399f93e8b2c1025b56fc836f0e193955cbefc
iluvpy <miomanns265@gmail.com>
2021-07-18 23:46:21 +0200

    added some getters

--- src/include/lexer.c
+++ src/include/lexer.c
@@ -3,11 +3,23 @@
 
 
 // transforms a mathematical expression into a simple start - to finish string of 
-// tokens seperated by spaces
+// tokens seperated by endlines
 cstr *get_tokens(cstr *m_expression) {
 
-	// find first to execute like parentethis (1+2)*3 1+2 needs to be executed before *3
-
-	// transform to tokens
+	
 	return ((cstr*)0);
+}
+
+
+cstr *get_int_token(int n) {
+	char *s = malloc(get_intlen(n)+1+strlen(TOKEN_INTEGER));
+	sprintf(s, "%s%d", TOKEN_INTEGER, n);
+	return cstr_from_allocstr(s);  
+	
+}
+
+cstr *get_float_token(float f) {
+	char *s = malloc(get_floatlen(f)+1+strlen(TOKEN_FLOAT));
+	sprintf(s, "%s%0.6f", TOKEN_FLOAT, f);
+	return cstr_from_allocstr(s);  
 }
--- src/include/lexer.h
+++ src/include/lexer.h
@@ -1,8 +1,28 @@
 #pragma once
 
+#include <stdio.h>
 #include "cstr.h"
+#include "utils.h"
+// operators 
+#define OP_PLUS "OP::+"
+#define OP_MINUS "OP::-"
+#define OP_DIVIDE "OP::/"
+#define OP_MULTIPLY "OP::*"
+#define OP_POW "OP::^"
 
+
+// parse info
+
+// tells the parser to execute everything that came before first
+// e.g INT::1 * INT::76 INFO::FIRST
+#define TEXEC_FIRST "INFO::FIRST"
+
+// types 
+#define TOKEN_INTEGER "INT::"
+#define TOKEN_FLOAT "FLOAT::"
 
 // transforms a mathematical expression into a simple start - to finish string of 
 // tokens seperated by spaces
 cstr *get_tokens(cstr *m_expression);
+cstr *get_int_token(int n);
+cstr *get_float_token(float d);
