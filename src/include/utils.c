9bac2e3cf090060c46a164e952fbc1f254237d33
iluvpy <miomanns265@gmail.com>
2021-07-18 23:47:38 +0200

    added useful functions

--- src/include/utils.c
+++ src/include/utils.c
@@ -7,3 +7,18 @@
 char *bool_str(bool b) {
     return b ? "True" : "False";
 }
+
+// returns length of integer
+size_t get_intlen(int n) {
+	size_t length = 0;
+	int n2 = 1;
+	while (n2 < n) {n2 *= 10; length++;}
+	return length;
+}
+
+// returns length of float
+// note this returns the int len of f plus 7 because we use 7 decimal points
+size_t get_floatlen(float f) {
+	int int_f = (int)f;
+	return get_intlen(int_f)+7; 
+}
