c1ffabcf433598b54aa936368fe5ec6b78008380
iluvpy <miomanns265@gmail.com>
2021-07-22 22:52:49 +0200

    added ivec files

---
+++ src/include/ivec.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdlib.h>
+
+typedef struct ivec {
+	int **vec;
+	size_t length;
+} ivec;
+
+#define IVEC_SIZE sizeof(ivec)
+#define IVEC_PTR_SIZE sizeof(ivec*)
+
+ivec *generate_ivec();
+void ivec_append(ivec *vec, int n);
+int ived_get(ivec *vec, size_t index);
+void del_ivec(ivec *vec);
