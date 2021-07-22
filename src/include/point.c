6f0504055b99428e90c786486bf480c2489f1611
iluvpy <miomanns265@gmail.com>
2021-07-22 13:48:06 +0200

    added point.c and point.h

---
+++ src/include/point.c
@@ -0,0 +1,13 @@
+#include "point.h"
+
+point *alloc_point() {
+	return malloc(sizeof(point));
+}
+point *get_point(int x, int y) {
+	point *p = alloc_point();
+	p->x = x;
+	p->y = y;
+	return p;
+}
+
+void free_point(point *p) {free(p);}
--- src/include/point.h
+++ src/include/point.h
@@ -1,5 +1,10 @@
 #pragma once 
+#include <stdlib.h>
 
 typedef struct point {
 	int x, y;
 } point;
+
+point *alloc_point();
+point *get_point(int x, int y);
+void free_point(point *p);
