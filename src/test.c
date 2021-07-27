df7d2c46d71e29209bf77222ba6f8c6fda133717
iluvpy <miomanns265@gmail.com>
2021-07-27 17:14:41 +0200

    removed unessesary tests

--- src/test.c
+++ src/test.c
@@ -20,56 +20,28 @@ void print_section(char *name) {
 
 int main(void) {
 
-	print_section("string vector test");
+	// print_section("string vector test");
 
-	svector *vec = generate_svec();
-	cstr *str = get_cstr("hello");
-	svec_appendc_str(vec, "hello");
-	svec_appendc_str(vec, "world");
-	svec_appendc_str(vec, "i like cheese");
-	svec_appendc_str(vec, "h4");
-	svec_appendc_str(vec, "hi");
-	svec_append(vec, str);
+	// svector *vec = generate_svec();
+	// cstr *str = get_cstr("hello");
+	// svec_appendc_str(vec, "hello");
+	// svec_appendc_str(vec, "world");
+	// svec_appendc_str(vec, "i like cheese");
+	// svec_appendc_str(vec, "h4");
+	// svec_appendc_str(vec, "hi");
+	// svec_append(vec, str);
 
-	for (int i = 0; i < svec_len(vec); i++) {
-		printf("e%d is equal to '%s'\n", i, cstr_str(svec_get(vec, i)));
-	}
-	del_svec(vec);
-	del_cstr(str);
-	
-	print_section("end of str vec test");
+	// for (int i = 0; i < svec_len(vec); i++) {
+	// 	printf("e%d is equal to '%s'\n", i, cstr_str(svec_get(vec, i)));
+	// }
+	// del_svec(vec);
+	// del_cstr(str);
 
-	int n = 152566;
-	printf("there are %zu digits in %d\n", get_intlen(n), n);
-
-	cstr *itoken = get_int_token(52342);
-	printf("int token: %s\n", cstr_str(itoken));
-	del_cstr(itoken);
-
-	cstr *ftoken = get_float_token(125.6534f);
-	printf("float token: %s\n", cstr_str(ftoken));
-	del_cstr(ftoken);
-
-	cstr *phrase = get_cstr("hello world.");
-	char c = '.';
-	printf("'%c' is in '%s': %s\n", c, cstr_str(phrase), bool_str(cstr_is_in(phrase, c)));
-	del_cstr(phrase);
-
-	cstr *text = get_cstr("");
-	for (int i = 0; i < 26; i++) {
-		cstr_appendc(text, i+97);
-	}
-	printf("result: ");
-	for (int i = 0; i < 26; i++) {
-		printf("%c", cstr_getc(text, i));
-	}
-	printf("\n");
-
-	char char_ = 97;
-	cstr_appendc(text, (int)char_);
-	printf("added '%c' to text variable\n", char_);
-	printf("text variable is now equal to '%s'\n", cstr_str(text));
-	del_cstr(text);
+	cstr *text2 = get_cstr("hello");
+	printf("text2 now: %s\n", cstr_str(text2));
+	text2 = cstr_delc(text2, 'l');
+	printf("text2 after removing 'l': %s\n", cstr_str(text2));
+	del_cstr(text2);
 
  	return 0;
 }
