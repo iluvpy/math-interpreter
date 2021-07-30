df3d221b28491939a73b3dd4b42e4cb1592ace99
iluvpy <miomanns265@gmail.com>
2021-07-30 23:24:04 +0200

    rewriting parser

--- src/include/parser.h
+++ src/include/parser.h
@@ -4,5 +4,6 @@
 #include "strvec.h"
 #include "lexer.h"
 #include "ast.h"
+#include "console.h"
 
 Ast *parser(svector *tokens);
--- src/parser.c
+++ src/parser.c
@@ -4,27 +4,9 @@
 // returns the result of the parsed tokens
 Ast *parser(svector *tokens) {
 	Ast *ast = alloc_ast();
-	for (int i = 0; i < svec_len(tokens); i++) {
-		cstr *token = svec_get(tokens, i);
+	AstNode *start_node = alloc_astNode();
+	ast_setNode(ast, start_node);
 
-		if (is_op_token(token)) {
-			printf("found operator token\n");
-			switch (get_op_char(token))
-			{
-				case '*':
-					printf("wow youre multiplying somethin'");
-					break;
-				case '/':
-					break;
-				
-				default:
-					break;
-			}
-		}
 
-		cstr *type = token_typeof(token);
-		printf("parser found token type of '%s'\n", cstr_str(type));
-		del_cstr(type);
-	}
 	return ast;
 }
