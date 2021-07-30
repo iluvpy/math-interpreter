8841713db1e5a0ca43473a92e0440b29506a8226
iluvpy <miomanns265@gmail.com>
2021-07-30 15:42:16 +0200

    added ast

--- src/ast.c
+++ src/ast.c
@@ -1,1 +1,66 @@
 #include "ast.h"
+
+// ast
+Ast *alloc_ast() {
+    Ast *ast = malloc(AST_SIZE);
+    ast->first_node = NULL;
+    return ast;
+}
+
+void ast_setNode(Ast* ast, AstNode *node) {
+    ast->first_node = node;
+}
+
+AstNode *ast_getNode(Ast *ast) {
+    return ast->first_node;
+}
+
+void del_ast(Ast *ast) {
+    del_node(ast->first_node);
+}
+
+
+// ast node
+AstNode *alloc_astNode() {
+    AstNode *node = malloc(ASTNODE_SIZE);
+    node->value = NULL;
+    node->left = NULL;
+    node->right = NULL; 
+    return node;
+}
+
+AstNode *get_node(cstr *value) {
+    AstNode *node = alloc_astNode();
+    node->value = value;
+    return node;
+}
+
+
+// copies value into node->value
+void node_setValue(AstNode *node, cstr *value) {
+    node->value = cstr_cpy(value);
+}
+
+void node_setLeft(AstNode *node, AstNode *left) {
+    node->left = left;
+}
+
+void node_setRight(AstNode *node, AstNode *right) {
+    node->right = right;
+}
+
+void del_node(AstNode *node) {
+    if (node != NULL) {
+        del_cstr(node->value);
+        if (node->left != NULL) {
+            del_node(node->left);
+            node->left = NULL;
+        }
+        if (node->right != NULL) {
+            del_node(node->right);
+            node->right = NULL; 
+        }
+        free(node);
+        node = NULL;
+    }
+} 
--- src/include/ast.h
+++ src/include/ast.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "cstr.h"
+
+#define AST_SIZE sizeof(Ast)
+#define ASTNODE_SIZE sizeof(AstNode)
+
+struct AstNode {
+    cstr *value;
+    struct AstNode *left;
+    struct AstNode *right;
+};
+typedef struct AstNode AstNode;
+
+typedef struct Ast {
+    AstNode *first_node;
+} Ast;
+
+
+// ast
+Ast *alloc_ast();
+void ast_setNode(Ast* ast, AstNode *node);
+AstNode *ast_getNode(Ast *ast);
+// frees all the memory in the ast
+void del_ast(Ast *ast);
+
+// ast node
+AstNode *alloc_astNode();
+AstNode *get_node(cstr *value);
+// copies value into node->value
+void node_setValue(AstNode *node, cstr *value); 
+void node_setLeft(AstNode *node, AstNode *left);
+void node_setRight(AstNode *node, AstNode *right);
+// frees itself and all the nodes below it
+void del_node(AstNode *node);
